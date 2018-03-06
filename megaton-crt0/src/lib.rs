5d3505840e42b2a0ee655428dd730e3a509bb0f7
roblabla <unfiltered@roblab.la>
2018-03-06 21:10:37 +0100

    Vrous fixes to crt0

--- megaton-crt0/src/lib.rs
+++ megaton-crt0/src/lib.rs
@@ -16,61 +16,77 @@ pub extern "C" fn entry() -> ! {
     loop {}
 }*/
 
-#![feature(global_asm, naked_functions, core_intrinsics, asm, lang_items, compiler_builtins_lib)]
+#![feature(global_asm, naked_functions, core_intrinsics, asm, lang_items, compiler_builtins_lib, untagged_unions)]
 #![no_std]
 
 extern crate compiler_builtins;
 
-use core::{ptr, slice, intrinsics};
+#[cfg(feature = "log")]
+extern crate core_io as io;
+#[cfg(feature = "log")]
+extern crate spin;
+
+use core::{slice, intrinsics};
 
 #[naked]
 #[no_mangle]
 #[link_section = ".text.jmp"]
 pub unsafe extern fn _start() -> ! {
-    asm!("b start
-          .word _mod_header - _start" : : : : "volatile");
+    asm!("
+    b trampoline
+   .word _mod_header - _start
+    " : : : : "volatile");
     intrinsics::unreachable();
 }
 
 #[naked]
 #[no_mangle]
 pub unsafe extern fn __svcExitProcess() -> ! {
+    // TODO: Clobbers
     asm!("svc 0x07" : : : : "volatile");
     intrinsics::unreachable();
 }
 
+// Make sure x30 is set to something.
 #[naked]
 #[no_mangle]
-pub unsafe extern fn start() -> ! {
+pub unsafe extern fn trampoline() -> ! {
+    // TODO: Clobber
     asm!("
-        // clear .bss
-        adrp x5, __bss_start
-        adrp x6, __bss_end
-
-        bssloop:
-            cmp x5, x6
-            b.eq run
-            str xzr, [x5]
-            add x5, x5, 8
-            b bssloop
-
-        run:
-            adrp x2, _start // aslr base
-
-            // set LR to svcExitProcess if it's null
-            adrp x3, __svcExitProcess
-            add x3, x3, #:lo12:__svcExitProcess
-            cmp x30, xzr
-            csel x30, x3, x30, eq
-
-            sub sp, sp, 0x10
-            stp x29, x30, [sp]
-            bl megaton_start
-            ldp x29, x30, [sp], 0x10
-            ret" : : : : "volatile");
+    adrp x3, __svcExitProcess
+    add x3, x3, #:lo12:__svcExitProcess
+    cmp x30, xzr
+    csel x30, x3, x30, eq
+    b start" : : : : "volatile");
     intrinsics::unreachable();
 }
 
+#[no_mangle]
+pub unsafe extern fn start(config: *mut LoaderConfigEntry, _thread_handle: u64) -> i32 {
+    // Clean the BSS.
+    // TODO: Avoid relocations by using asm!. This is **ugly**.
+    asm!("
+	// clear .bss
+	adrp x5, __bss_start
+	adrp x6, __bss_end
+
+bssloop:
+	cmp x5, x6
+	b.eq run
+	str xzr, [x5]
+	add x5, x5, 8
+	b bssloop
+run:
+    " : : : "x5", "x6" : "volatile");
+
+    let start_addr : *mut ();
+    // TODO: Avoid relocations by getting _start address with asm...
+    asm!("adrp $0, _start" : "=r"(start_addr));
+    let ret = megaton_start(config, _thread_handle, start_addr);
+    ret
+}
+
+// TODO: I should try to get rid of that one last bit of asm.
 #[link_section = ".data.mod0"]
 global_asm!(r#"
 .global _mod_header
@@ -86,34 +102,119 @@ IS_NRO:
     .word 1
 "#);
 
-pub struct LoaderConfig;
+mod relocation;
 
-#[derive(Debug)]
-struct DynInfo<'a, 'b> {
-    init_array: Option<&'a [extern fn()]>,
-    fini_array: Option<&'b [extern fn()]>,
+/*pub enum LoaderConfigEntry {
+    EndOfList = 0,
+    MainThreadHandle(u32),
+    NextLoadPath(*const u8, *const u8),
+    OverrideHeap(*const (), usize),
+    OverrideService(u64, u32),
+    Argv(u64, *const *const u8),
+    SyscallAvailableHint([u8; 16]),
+    AppletType(u64),
+    StdioSockets(u32, u32, u32, u64),
+    ProcessHandle(u32),
+    LastLoadResult(u32),
+    AllocPages,
+    LockRegion(*const (), usize),
+    Log(*const u8, usize) = 51
+}*/
+
+mod LoaderConfigTag {
+    pub const EndOfList: u32 = 0;
+    pub const MainThreadHandle: u32 = 1;
+    pub const NextLoadPath: u32 = 2;
+    pub const OverrideHeap: u32 = 3;
+    pub const OverrideService: u32 = 4;
+    pub const Argv: u32 = 5;
+    pub const SyscallAvailableHint: u32 = 6;
+    pub const AppletType: u32 = 7;
+    pub const StdioSockets: u32 = 8;
+    pub const ProcessHandle: u32 = 9;
+    pub const LastLoadResult: u32 = 10;
+    pub const AllocPages: u32 = 11;
+    pub const LockRegion: u32 = 12;
+    pub const Log: u32 = 51;
+}
+
+#[repr(C)]
+union LoaderConfigEntryData {
+    // TODO: All of them.
+    log: (*const u8, usize)
+
+}
+
+#[repr(C)]
+pub struct LoaderConfigEntry {
+    tag: u32,
+    flags: u32,
+    data: (u64, u64)
 }
 
 extern {
-    fn main(argc: isize, argv: *const *const u8) -> u32;
+    fn main(argc: isize, argv: *const *const u8) -> i32;
+}
+
+// TODO: Should this be here? I need to clean up who is responsible for what.
+// TODO: THIS IS TERRIFYING!
+#[cfg(feature = "log")]
+static mut LOG : spin::Mutex<io::Cursor<&'static mut [u8]>> = unsafe { JUNK.data };
+
+#[cfg(feature = "log")]
+union TerrifyingUnion {
+    data: spin::Mutex<io::Cursor<&'static mut [u8]>>,
+    junk: u32
+}
+
+#[cfg(feature = "log")]
+const JUNK: TerrifyingUnion = TerrifyingUnion { junk: 0 };
+
+#[cfg(feature = "log")]
+pub fn write_to_log(s: &str) {
+    use io::Write;
+
+    unsafe { LOG.lock().write(s.as_bytes()); }
 }
 
 #[no_mangle]
-pub unsafe extern fn megaton_start(_config: *mut LoaderConfig, _thread_handle: u64, aslr_base: *mut ()) {
-    let mut dyn_info = DynInfo {
+unsafe extern fn megaton_start(mut config: *mut LoaderConfigEntry, _thread_handle: u64, aslr_base: *mut ()) -> i32 {
+    let mut dyn_info = relocation::DynInfo {
         init_array: None,
         fini_array: None
     };
 
-    if let Err(_) = relocate(aslr_base as _, &mut dyn_info) {
-        return;
+    if let Err(x) = relocation::relocate(aslr_base as _, &mut dyn_info) {
+        return -(x as i32);
     }
+
+    loop {
+        match (*config).tag {
+            LoaderConfigTag::EndOfList => break,
+            LoaderConfigTag::Log => {
+                #[cfg(feature = "log")]
+                {
+                    LOG = spin::Mutex::new(io::Cursor::new(slice::from_raw_parts_mut((*config).data.0 as _, (*config).data.1 as _)));
+                }
+            },
+            _ => {
+                if (*config).flags & 1 == 0 {
+                    // Is mandatory! Let's suicide.
+                    return -2;
+                }
+            }
+        }
+        config = config.offset(1);
+    }
+    // Handle Loader Config
 
     // TODO: Might want to run init_array 👀
 
     // The arguments are in the config, but I'm a bit too lazy to try it right
     // now.
     let _ret = main(0, core::ptr::null());
+
+    _ret
 }
 
 #[lang = "termination"]
@@ -139,169 +240,4 @@ T: Termination,
 #[lang = "panic_fmt"]
 fn panic_fmt(_msg: core::fmt::Arguments, _file: &'static str, _line: u32) -> ! {
     loop {}
-}
-
-// -- Relocation stuff --
-#[repr(C)]
-struct ModuleHeader {
-    magic: u32,
-    dynamic_off: u32,
-    bss_start_off: u32,
-    bss_end_off: u32,
-    unwind_start_off: u32,
-    unwind_end_off: u32,
-    module_object_off: u32
-}
-
-#[repr(C)]
-struct Elf64Dyn {
-    tag: i64,
-    val: u64, // Who cares about the union ಠ_ಠ
-}
-
-#[repr(C)]
-struct Elf64Rela {
-    offset: u64,
-    reloc_type: u32,
-    symbol: u32,
-    addend: u64
-}
-
-const DT_RELA: i64 = 7;
-const DT_RELASZ: i64 = 8;
-const DT_RELAENT: i64 = 9;
-const DT_INIT_ARRAY: i64 = 25;
-const DT_FINI_ARRAY: i64 = 26;
-const DT_INIT_ARRAYSZ: i64 = 27;
-const DT_FINI_ARRAYSZ: i64 = 28;
-const DT_RELACOUNT: i64 = 0x6ffffff9;
-
-const R_AARCH64_RELATIVE: u32 = 0x403;
-
-/// Do the dynamic linker's job. Should be the first function called.
-unsafe fn relocate(aslr_base: *mut u8, dyn_info: &mut DynInfo) -> Result<(), ()> {
-    let mod_header = aslr_base.offset(*(aslr_base.offset(4) as *const u32) as isize) as *mut ModuleHeader;
-    let mod_ptr = mod_header as *const u8;
-
-
-    let mut rela_offset = None;
-    let mut rela_size = 0;
-    let mut rela_ent = 0;
-    let mut rela_count = 0;
-    let mut init_array = ptr::null() as *const extern fn();
-    let mut init_array_sz = None;
-    let mut fini_array = ptr::null() as *const extern fn();
-    let mut fini_array_sz = None;
-
-    // Ensure we have a valid MOD0 header
-    let mod_magic = 0x30444f4d; // 'MOD0'
-    if (*mod_header).magic != mod_magic {
-        return Err(());
-    }
-
-    // Traverse the dynamic section, extract all relevant information.
-    let mut dynamic = mod_ptr.offset((*mod_header).dynamic_off as isize) as *const Elf64Dyn;
-    while (*dynamic).tag > 0 {
-        match (*dynamic).tag {
-            DT_RELA => {
-                if rela_offset.is_some() {
-                    return Err(());
-                }
-                rela_offset = Some((*dynamic).val);
-            },
-            DT_RELASZ => {
-                rela_size = (*dynamic).val;
-            }
-            DT_RELAENT => {
-                rela_ent = (*dynamic).val;
-            }
-            DT_INIT_ARRAY => {
-                if !init_array.is_null() {
-                    return Err(());
-                }
-                init_array = aslr_base.offset((*dynamic).val as isize) as _;
-            },
-            DT_FINI_ARRAY => {
-                if !fini_array.is_null() {
-                    return Err(());
-                }
-                fini_array = aslr_base.offset((*dynamic).val as isize) as _;
-            }
-            DT_INIT_ARRAYSZ => {
-                if init_array_sz.is_some() {
-                    return Err(())
-                }
-                init_array_sz = Some((*dynamic).val);
-            }
-            DT_FINI_ARRAYSZ => {
-                if fini_array_sz.is_some() {
-                    return Err(())
-                }
-                fini_array_sz = Some((*dynamic).val);
-            },
-            DT_RELACOUNT => {
-                rela_count = (*dynamic).val;
-            },
-            _ => {
-                // UNHANDLED! We *might* want to log this, but we can't safely
-                // access anything.
-            }
-        }
-        dynamic = dynamic.offset(1);
-    }
-
-    // Save init_array/fini_array somewhere the crt0 can find.
-    match (init_array, init_array_sz) {
-        (p, Some(sz)) if !p.is_null() => {
-            dyn_info.init_array = Some(slice::from_raw_parts(p, sz as usize));
-        },
-        (p, None) if p.is_null() => (),
-        _ => {
-            // We had an INIT_ARRAY but no INIT_ARRAYSZ. What ?
-            return Err(())
-        }
-    }
-    match (fini_array, fini_array_sz) {
-        (p, Some(sz)) if !p.is_null() => {
-            dyn_info.fini_array = Some(slice::from_raw_parts(p, sz as usize));
-        },
-        (p, None) if p.is_null() => (),
-        _ => {
-            // We had a FINI_ARRAY but no FINI_ARRAYSZ. What ?
-            return Err(())
-        }
-    }
-
-    // TODO: Comment explaining why ?
-    if rela_ent != 0x18 {
-        return Err(());
-    }
-
-    if rela_size != rela_count * rela_ent {
-        return Err(());
-    }
-
-    // Handle relocations. On the switch, there is no dynamic linker: it's up to
-    // the process to link itself correctly.
-    if let Some(rela_offset) = rela_offset {
-        let rela_base = (aslr_base.offset(rela_offset as isize)) as *const Elf64Rela;
-        for i in 0..rela_count {
-            let rela = rela_base.offset(i as isize);
-
-            match (*rela).reloc_type {
-                R_AARCH64_RELATIVE => {
-                    if (*rela).symbol != 0 {
-                        return Err(());
-                    }
-                    *(aslr_base.offset((*rela).offset as isize) as *mut *mut ()) = aslr_base.offset((*rela).addend as isize) as _;
-                    break;
-                },
-                _ => return Err(())
-            }
-        }
-    } else {
-        return Err(())
-    }
-
-    return Ok(());
 }
---
+++ megaton-crt0/src/relocation.rs
@@ -0,0 +1,175 @@
+use core::{ptr, slice};
+
+#[derive(Debug)]
+pub struct DynInfo<'a, 'b> {
+    pub init_array: Option<&'a [extern fn()]>,
+    pub fini_array: Option<&'b [extern fn()]>,
+}
+
+#[repr(C)]
+struct ModuleHeader {
+    magic: u32,
+    dynamic_off: u32,
+    bss_start_off: u32,
+    bss_end_off: u32,
+    unwind_start_off: u32,
+    unwind_end_off: u32,
+    module_object_off: u32
+}
+
+#[repr(C)]
+struct Elf64Dyn {
+    tag: i64,
+    val: u64, // Who cares about the union ಠ_ಠ
+}
+
+#[repr(C)]
+struct Elf64Rela {
+    offset: u64,
+    reloc_type: u32,
+    symbol: u32,
+    addend: u64
+}
+
+const DT_RELA: i64 = 7;
+const DT_RELASZ: i64 = 8;
+const DT_RELAENT: i64 = 9;
+const DT_INIT_ARRAY: i64 = 25;
+const DT_FINI_ARRAY: i64 = 26;
+const DT_INIT_ARRAYSZ: i64 = 27;
+const DT_FINI_ARRAYSZ: i64 = 28;
+const DT_RELACOUNT: i64 = 0x6ffffff9;
+
+const R_AARCH64_RELATIVE: u32 = 0x403;
+
+/// Do the dynamic linker's job. Should be the first function called.
+#[inline(always)]
+pub unsafe fn relocate(aslr_base: *mut u8, dyn_info: &mut DynInfo) -> Result<(), u32> {
+    let mod_header = aslr_base.offset(*(aslr_base.offset(4) as *const u32) as isize) as *mut ModuleHeader;
+    let mod_ptr = mod_header as *const u8;
+
+
+    let mut rela_offset = None;
+    let mut rela_size = 0;
+    let mut rela_ent = 0;
+    let mut rela_count = 0;
+    let mut init_array = ptr::null() as *const extern fn();
+    let mut init_array_sz = None;
+    let mut fini_array = ptr::null() as *const extern fn();
+    let mut fini_array_sz = None;
+
+    // Ensure we have a valid MOD0 header
+    let mod_magic = 0x30444f4d; // 'MOD0'
+    if (*mod_header).magic != mod_magic {
+        return Err(13);
+    }
+
+    // Traverse the dynamic section, extract all relevant information.
+    let mut dynamic = mod_ptr.offset((*mod_header).dynamic_off as isize) as *const Elf64Dyn;
+    while (*dynamic).tag > 0 {
+        match (*dynamic).tag {
+            DT_RELA => {
+                if rela_offset.is_some() {
+                    return Err(1);
+                }
+                rela_offset = Some((*dynamic).val);
+            },
+            DT_RELASZ => {
+                rela_size = (*dynamic).val;
+            }
+            DT_RELAENT => {
+                rela_ent = (*dynamic).val;
+            }
+            DT_INIT_ARRAY => {
+                if !init_array.is_null() {
+                    return Err(2);
+                }
+                init_array = aslr_base.offset((*dynamic).val as isize) as _;
+            },
+            DT_FINI_ARRAY => {
+                if !fini_array.is_null() {
+                    return Err(3);
+                }
+                fini_array = aslr_base.offset((*dynamic).val as isize) as _;
+            }
+            DT_INIT_ARRAYSZ => {
+                if init_array_sz.is_some() {
+                    return Err(4)
+                }
+                init_array_sz = Some((*dynamic).val);
+            }
+            DT_FINI_ARRAYSZ => {
+                if fini_array_sz.is_some() {
+                    return Err(5)
+                }
+                fini_array_sz = Some((*dynamic).val);
+            },
+            DT_RELACOUNT => {
+                rela_count = (*dynamic).val;
+            },
+            _ => {
+                // UNHANDLED! We *might* want to log this, but we can't safely
+                // access anything.
+            }
+        }
+        dynamic = dynamic.offset(1);
+    }
+
+    // TODO: Comment explaining why ?
+    if rela_ent != 0x18 {
+        return Err(6);
+    }
+
+    if rela_size != rela_count * rela_ent {
+        return Err(7);
+    }
+
+    // Handle relocations. On the switch, there is no dynamic linker: it's up to
+    // the process to link itself correctly.
+    if let Some(rela_offset) = rela_offset {
+        let rela_base = (aslr_base.offset(rela_offset as isize)) as *const Elf64Rela;
+        for i in 0..rela_count {
+            let rela = rela_base.offset(i as isize);
+
+            match (*rela).reloc_type {
+                R_AARCH64_RELATIVE => {
+                    if (*rela).symbol != 0 {
+                        return Err(8);
+                    }
+                    *(aslr_base.offset((*rela).offset as isize) as *mut *mut ()) = aslr_base.offset((*rela).addend as isize) as _;
+                    break;
+                },
+                _ => return Err(9)
+            }
+        }
+    } else {
+        return Err(10)
+    }
+
+    // Relocations are handled! Calling functions should be fine, so long as
+    // they do not rely on data being initialized.
+
+    // Save init_array/fini_array somewhere the crt0 can find.
+    match (init_array, init_array_sz) {
+        (p, Some(sz)) if !p.is_null() => {
+            dyn_info.init_array = Some(slice::from_raw_parts(p, sz as usize));
+        },
+        (p, None) if p.is_null() => (),
+        _ => {
+            // We had an INIT_ARRAY but no INIT_ARRAYSZ. What ?
+            return Err(11)
+        }
+    }
+    match (fini_array, fini_array_sz) {
+        (p, Some(sz)) if !p.is_null() => {
+            dyn_info.fini_array = Some(slice::from_raw_parts(p, sz as usize));
+        },
+        (p, None) if p.is_null() => (),
+        _ => {
+            // We had a FINI_ARRAY but no FINI_ARRAYSZ. What ?
+            return Err(12)
+        }
+    }
+
+    return Ok(());
+}
